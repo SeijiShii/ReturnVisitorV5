ad8b8ef6b6e4b19ad22216a5c5ea55d32fbc6d4f
Seiji Shii <seiji.shii@gmail.com>
2017-02-15 20:43:31 +0900

    マップの表示地点を保存する

---
+++ app/src/main/java/net/c_kogyo/returnvisitorv5/activities/Constants.java
@@ -0,0 +1,105 @@
+package net.c_kogyo.returnvisitorv5.activities;
+
+//import net.c_kogyo.returnvisitor.R;
+
+/**
+ * Created by SeijiShii on 2016/07/24.
+ */
+
+public class Constants {
+
+    // Shared Preferences用のタグ類
+    public static final class SharedPrefTags{
+
+        public static final String RETURN_VISITOR_SHARED_PREFS = "return_visitor_shared_prefs";
+        static final String ZOOM_LEVEL = "zoom_level";
+        static final String LATITUDE = "latitude";
+        static final String LONGITUDE = "longitude";
+//        public static final String USER_NAME = "user_name";
+    }
+
+
+//    public static final int[] buttonRes = {
+//            R.mipmap.button_gray,
+//            R.mipmap.button_red,
+//            R.mipmap.button_purple,
+//            R.mipmap.button_blue,
+//            R.mipmap.button_green,
+//            R.mipmap.button_yellow,
+//            R.mipmap.button_orange
+//    } ;
+//
+//    public static final int[] markerRes = {
+//            R.mipmap.marker,
+//            R.mipmap.marker_red,
+//            R.mipmap.marker_purple,
+//            R.mipmap.marker_blue,
+//            R.mipmap.marker_green,
+//            R.mipmap.marker_yellow,
+//            R.mipmap.marker_orange
+//    };
+
+    public static final String DATE_LONG = "date_data";
+
+    public static final String REQUEST_CODE = "Request Code";
+
+//    public class PersonCode {
+//
+//        public static final int ADD_PERSON_REQUEST_CODE = 1000;
+//        public static final int EDIT_PERSON_REQUEST_CODE = 1001;
+//
+//        public static final int PERSON_ADDED_RESULT_CODE = 1002;
+//        public static final int PERSON_EDITED_RESULT_CODE = 1003;
+//        public static final int PERSON_CANCELED_RESULT_CODE = 1004;
+//
+//    }
+//
+//    public class PlacementCode {
+//
+//        public static final int PLACEMENT_REQUEST_CODE = 2000;
+//        public static final int PLACEMENT_ADDED_RESULT_CODE = 2010;
+//        public static final int PLACEMENT_CANCELED_RESULT_CODE = 2020;
+//    }
+//
+//    public class LogInCode {
+//
+//        public static final int GOOGLE_SIGN_IN_RC = 3000;
+////        public static final int FACEBOOK_LOG_IN_RC = 3001;
+//
+//
+//    }
+//
+//    public class RecordVisitActions {
+//
+//        public static final String NEW_PLACE_ACTION             = "new_place_action";
+//        public static final String NEW_VISIT_ACTION_WITH_PLACE  = "new_visit_action_with_place";
+//        public static final String EDIT_VISIT_ACTION            = "edit_visit_action";
+//        public static final String NEW_VISIT_ACTION_NO_PLACE    = "new_visit_action_no_place";
+//
+//        public static final int EDIT_VISIT_REQUEST_CODE = 4000;
+//        public static final int NEW_VISIT_REQUEST_CODE = 4001;
+//
+//        public static final int DELETE_VISIT_RESULT_CODE = 4010;
+//        public static final int VISIT_CHANGED_RESULT_CODE = 4020;
+//        public static final int VISIT_ADDED_RESULT_CODE = 4030;
+//
+//
+//
+//    }
+//
+//    public class CalendarActions {
+//
+//        public static final String START_CALENDAR_FROM_WORK_ACTION = "start_calendar_from_work_action";
+//        public static final String START_CALENDAR_FROM_MAP_ACTION = "start_calendar_from_map_action";
+//        public static final int START_CALENDAR_REQUEST_CODE = 5000;
+//        public static final int PRESS_DATE_RESULT_CODE = 5010;
+//    }
+//
+//    public class WorkActivityActions {
+//
+//        public static final String SHOW_GENERAL_WORKS = "show_general_works";
+//        public static final String POST_DELETE_VISIT = "post_delete_visit";
+//
+//    }
+
+}
---
+++ app/src/main/java/net/c_kogyo/returnvisitorv5/activities/MainActivity.java
@@ -0,0 +1,241 @@
+package net.c_kogyo.returnvisitorv5.activities;
+
+import android.Manifest;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import net.c_kogyo.returnvisitorv5.R;
+
+import static net.c_kogyo.returnvisitorv5.activities.Constants.SharedPrefTags.LATITUDE;
+import static net.c_kogyo.returnvisitorv5.activities.Constants.SharedPrefTags.LONGITUDE;
+import static net.c_kogyo.returnvisitorv5.activities.Constants.SharedPrefTags.RETURN_VISITOR_SHARED_PREFS;
+import static net.c_kogyo.returnvisitorv5.activities.Constants.SharedPrefTags.ZOOM_LEVEL;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+
+    private static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.map_activity);
+
+        initMapView(savedInstanceState);
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Bundle mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY);
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle);
+        }
+
+        mapView.onSaveInstanceState(mapViewBundle);
+    }
+
+    private MapView mapView;
+    private void initMapView(Bundle savedInstanceState){
+        mapView = (MapView) findViewById(R.id.map_view);
+
+        // *** IMPORTANT ***
+        // MapView requires that the Bundle you pass contain _ONLY_ MapView SDK
+        // objects or sub-Bundles.
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
+        }
+        mapView.onCreate(mapViewBundle);
+
+        mapView.getMapAsync(this);
+    }
+
+    private static final String MY_LOCATION_TAG = "my_location";
+    private GoogleMap mMap;
+    @Override
+    public void onMapReady(GoogleMap map) {
+
+        mMap = map;
+
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setZoomGesturesEnabled(true);
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
+        // Permissionの扱いが変化するため
+        try {
+            mMap.setMyLocationEnabled(true);
+        } catch (SecurityException e) {
+
+            Log.d(MY_LOCATION_TAG,e.getMessage());
+        }
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+
+        if (Build.VERSION.SDK_INT >= 23) {
+
+            setMyLocationButton();
+        }
+
+        // xmlでの指定の方法が分からん
+        mMap.setPadding(0, 40, 0, 0);
+
+        loadCameraPosition();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+
+        saveCameraPosition();
+
+        try {
+
+            mMap.setMyLocationEnabled(false);
+        } catch (SecurityException e) {
+            //
+        }
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    private void loadCameraPosition() {
+        SharedPreferences prefs = getSharedPreferences(RETURN_VISITOR_SHARED_PREFS, MODE_PRIVATE);
+        float zoomLevel = prefs.getFloat(ZOOM_LEVEL, 0f);
+        double lat = Double.valueOf(prefs.getString(LATITUDE, "1000"));
+        double lng = Double.valueOf(prefs.getString(LONGITUDE, "1000"));
+
+        if (lat >= 1000 || lng >= 1000) return;
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lat, lng), zoomLevel));
+    }
+
+    private void saveCameraPosition() {
+
+        SharedPreferences prefs = getSharedPreferences(RETURN_VISITOR_SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+
+        float zoomLevel = mMap.getCameraPosition().zoom;
+        String latSt = String.valueOf(mMap.getCameraPosition().target.latitude);
+        String lngSt = String.valueOf(mMap.getCameraPosition().target.longitude);
+
+        editor.putFloat(ZOOM_LEVEL, zoomLevel);
+        editor.putString(LATITUDE, latSt);
+        editor.putString(LONGITUDE, lngSt);
+
+        editor.apply();
+    }
+
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_LOCATION = 717;
+    private void setMyLocationButton() {
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            Log.d(MY_LOCATION_TAG, "Permissions yet given.");
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    || ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_COARSE_LOCATION)) {
+
+                Log.d(MY_LOCATION_TAG, "Should show Explanation.");
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_ACCESS_LOCATION);
+
+                // MY_PERMISSIONS_REQUEST_ACCESS_LOCATION is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                    Log.d(MY_LOCATION_TAG, "YES! permissions given.");
+
+                    try {
+                        mMap.setMyLocationEnabled(true);
+                    } catch (SecurityException e) {
+                        Log.d(MY_LOCATION_TAG,e.getMessage());
+                    }
+                    mMap.getUiSettings().setMyLocationButtonEnabled(true);
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+}
