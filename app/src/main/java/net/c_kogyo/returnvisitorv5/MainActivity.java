44f31c4aca820bbfdfd6ee083a5f9788baf480cf
Seiji Shii <seiji.shii@gmail.com>
2017-02-15 18:20:25 +0900

    Initial commit

---
+++ app/src/main/java/net/c_kogyo/returnvisitorv5/MainActivity.java
@@ -0,0 +1,12 @@
+package net.c_kogyo.returnvisitorv5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
