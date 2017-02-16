d76f8706578e02864796107b6f05b0a29592cabe
Seiji Shii <seiji.shii@gmail.com>
2017-02-16 17:32:54 +0900

    record visit mock 2 buttons

--- app/src/main/java/net/c_kogyo/returnvisitorv5/activities/RecordVisitActivity.java
+++ app/src/main/java/net/c_kogyo/returnvisitorv5/activities/RecordVisitActivity.java
@@ -1,10 +1,21 @@
 package net.c_kogyo.returnvisitorv5.activities;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import net.c_kogyo.returnvisitorv5.R;
 
 /**
  * Created by SeijiShii on 2017/02/16.
  */
 
 public class RecordVisitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.record_visit_activity);
+    }
 }
