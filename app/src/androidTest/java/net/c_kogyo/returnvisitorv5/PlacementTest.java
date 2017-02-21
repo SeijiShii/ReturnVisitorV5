9751c77d406c522f5126fac27845ffa8c8166bbc
Seiji Shii <seiji.shii@gmail.com>
2017-02-21 19:13:53 +0900

    Added PlacementTest

--- app/src/androidTest/java/net/c_kogyo/returnvisitorv5/PlacementTest.java
+++ app/src/androidTest/java/net/c_kogyo/returnvisitorv5/PlacementTest.java
@@ -1,8 +1,29 @@
 package net.c_kogyo.returnvisitorv5;
 
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import net.c_kogyo.returnvisitorv5.data.Placement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.*;
+
+import java.util.Calendar;
+
 /**
  * Created by SeijiShii on 2017/02/20.
  */
-
+@RunWith(AndroidJUnit4.class)
 public class PlacementTest {
+
+    @Test
+    public void assertNumberString() {
+        Context context = InstrumentationRegistry.getTargetContext();
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.MONTH, 0);
+        String numberString = Placement.getNumberString(cal, Placement.MagazineCategory.AWAKE, context);
+        assertEquals("Number String Test:", numberString, "No. 1 2017");
+    }
 }
