5a515d38e5e2cdc4db759952f234186a73582784
Seiji Shii <seiji.shii@gmail.com>
2017-03-05 17:54:08 +0900

    2ButtonFrame オーバーレイにタッチリスナを実装する

--- app/src/main/java/net/c_kogyo/returnvisitorv5/activity/RecordVisitActivity.java
+++ app/src/main/java/net/c_kogyo/returnvisitorv5/activity/RecordVisitActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
@@ -101,6 +102,13 @@ public class RecordVisitActivity extends AppCompatActivity {
     private RelativeLayout twoButtonsFrame;
     private void initTwoButtonsFrame() {
         twoButtonsFrame = (RelativeLayout) findViewById(R.id.two_buttons_frame);
+        // オーバーレイをタッチが透過するのを防ぐ
+        twoButtonsFrame.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                return true;
+            }
+        });
 
         initVisitDetailButton();
     }
