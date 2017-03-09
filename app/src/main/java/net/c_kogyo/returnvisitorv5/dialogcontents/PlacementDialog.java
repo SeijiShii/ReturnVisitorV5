9f1659b9b7f55c5c29b48f6a0cf49410c983f96d
Seiji Shii <seiji.shii@gmail.com>
2017-03-09 20:30:50 +0900

    Added Placement Dialog layout

---
+++ app/src/main/java/net/c_kogyo/returnvisitorv5/dialogcontents/PlacementDialog.java
@@ -0,0 +1,24 @@
+package net.c_kogyo.returnvisitorv5.dialogcontents;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+/**
+ * Created by SeijiShii on 2017/03/09.
+ */
+
+public class PlacementDialog extends FrameLayout {
+
+    public PlacementDialog(@NonNull Context context) {
+        super(context);
+    }
+
+    public PlacementDialog(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+}
