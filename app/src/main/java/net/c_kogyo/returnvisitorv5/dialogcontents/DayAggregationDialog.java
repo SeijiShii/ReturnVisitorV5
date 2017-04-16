5ba8aaefed0c4492edf205ce8c55443fc5bb06f8
Seiji Shii <seiji.shii@gmail.com>
2017-04-16 22:58:41 +0900

    Added day aggregation dialog layout

---
+++ app/src/main/java/net/c_kogyo/returnvisitorv5/dialogcontents/DayAggregationDialog.java
@@ -0,0 +1,24 @@
+package net.c_kogyo.returnvisitorv5.dialogcontents;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+/**
+ * Created by SeijiShii on 2017/04/16.
+ */
+
+public class DayAggregationDialog extends FrameLayout {
+
+    public DayAggregationDialog(@NonNull Context context) {
+        super(context);
+    }
+
+    public DayAggregationDialog(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+}
