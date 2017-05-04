85a1674691aa87915e202e6249aaa7f2a30eabf2
Seiji Shii <seiji.shii@gmail.com>
2017-05-04 17:34:20 +0900

    Added Calendar pager activity mock

---
+++ app/src/main/java/net/c_kogyo/returnvisitorv5/activity/CalendarPagerActivity.java
@@ -0,0 +1,10 @@
+package net.c_kogyo.returnvisitorv5.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by SeijiShii on 2017/05/04.
+ */
+
+public class CalendarPagerActivity extends AppCompatActivity {
+}
