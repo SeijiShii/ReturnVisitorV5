f9931e77f5baa7fc9f65911dc41b6f0d839c2190
Seiji Shii <seiji.shii@gmail.com>
2017-05-04 16:34:18 +0900

    Exception not sure if will be used

---
+++ app/src/main/java/net/c_kogyo/returnvisitorv5/util/ViewUtilExceptions.java
@@ -0,0 +1,14 @@
+package net.c_kogyo.returnvisitorv5.util;
+
+/**
+ * Created by SeijiShii on 2017/04/20.
+ */
+
+public class ViewUtilExceptions {
+
+//    class ConstructorNotImplementedException extends Exception {
+//        public ConstructorNotImplementedException(String viewClassName) {
+//            throw
+//        }
+//    }
+}
