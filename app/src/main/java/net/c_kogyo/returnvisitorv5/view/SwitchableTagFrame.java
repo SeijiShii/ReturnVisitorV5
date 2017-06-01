79a597af7118f4beefe2ae20b3a588d9737310ed
Seiji Shii <seiji.shii@gmail.com>
2017-06-01 22:50:46 +0900

    Fixed

--- app/src/main/java/net/c_kogyo/returnvisitorv5/view/SwitchableTagFrame.java
+++ app/src/main/java/net/c_kogyo/returnvisitorv5/view/SwitchableTagFrame.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
@@ -17,33 +18,25 @@ import java.util.ArrayList;
  * Created by SeijiShii on 2017/06/01.
  */
 
-public class SwitchableTagFrame extends LinearLayout {
-
-    private final static int LINE_HEIGHT = 35;
-    private final static int MARGIN = 5;
-
-    private int lineHeight, frameWidth, margin;
+public class SwitchableTagFrame extends TagLinesFrame {
 
     private ArrayList<String> mAllTagIds;
     private ArrayList<String> mSelectedTagIds;
     private ArrayList<Tag> mAllTags;
     private SwitchableTagFrameListener mListener;
 
-    public SwitchableTagFrame(Context context,
-                              ArrayList<String> allTagIds,
-                              ArrayList<String> selectedTagIds,
-                              SwitchableTagFrameListener callback) {
+    public SwitchableTagFrame(Context context) {
         super(context);
 
-        this.mAllTagIds = new ArrayList<>(allTagIds);
-        this.mSelectedTagIds = new ArrayList<>(selectedTagIds);
-        this.mListener = callback;
-
-        initCommon();
     }
 
     public SwitchableTagFrame(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+    }
+
+    @Override
+    public void setLayoutParams(TagLinesFrame frame) {
+        frame.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
     }
 
     public void setTagIdsAndInitialize(ArrayList<String> allTagIds,
@@ -54,19 +47,6 @@ public class SwitchableTagFrame extends LinearLayout {
         this.mSelectedTagIds = new ArrayList<>(selectedTagIds);
         this.mListener = callback;
 
-        initCommon();
-    }
-
-    private void initCommon() {
-
-        this.removeAllViews();
-
-        lineHeight = (int) (getContext().getResources().getDisplayMetrics().density * LINE_HEIGHT);
-        margin = (int) (getContext().getResources().getDisplayMetrics().density * MARGIN);
-
-        this.setOrientation(VERTICAL);
-        this.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-
         if (mAllTagIds.size() <= 0) {
             return;
         }
@@ -76,59 +56,14 @@ public class SwitchableTagFrame extends LinearLayout {
             return;
         }
 
-        waitAndSetWidth();
-    }
-
-    private LinearLayout generateNewLine() {
-
-        LinearLayout newLine = new LinearLayout(getContext());
-        newLine.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, lineHeight));
-        newLine.setOrientation(HORIZONTAL);
-
-        return newLine;
-    }
-
-    private void waitAndSetWidth() {
-
-//        setLayoutParams(new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-
-        final Handler handler = new Handler();
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (getWidth() <= 0) {
-                    try {
-                        Thread.sleep(50);
-                    } catch (InterruptedException e) {
-
-                    }
-                }
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        frameWidth = getWidth();
-                        putTagsInLine();
-                    }
-                });
-            }
-        }).start();
-
-    }
-
-    private void putTagsInLine() {
-
-        this.removeAllViews();
-        this.addView(generateNewLine());
-
-        int widthSum = 0;
-
-        for (Tag tag: mAllTags) {
+        ArrayList<View> tagViews = new ArrayList<>();
+        for (Tag tag : mAllTags) {
 
             SmallTagToggleButton tagToggleButton
                     = new SmallTagToggleButton(getContext(),
-                                tag,
-                                mSelectedTagIds.contains(tag.getId()),
-                                new SmallTagToggleButton.CheckChangeListener() {
+                    tag,
+                    mSelectedTagIds.contains(tag.getId()),
+                    new SmallTagToggleButton.CheckChangeListener() {
                         @Override
                         public void onCheckChange(Tag tag1, boolean checked) {
                             if (mSelectedTagIds.contains(tag1.getId())) {
@@ -145,17 +80,10 @@ public class SwitchableTagFrame extends LinearLayout {
                             }
                         }
                     });
-            if (tagToggleButton.getViewWidth() + margin + widthSum > frameWidth) {
-
-                // これから追加するタグの幅とすでにセットされているタグの幅を足したとき、フレームの幅を超えるようなら行を追加する
-                this.addView(generateNewLine());
-                widthSum = 0;
-            }
-            // 最後の行にタグを追加する
-            ((LinearLayout)(this.getChildAt(this.getChildCount() - 1))).addView(tagToggleButton);
-            widthSum += (tagToggleButton.getViewWidth() + margin);
+            tagViews.add(tagToggleButton);
         }
 
+        super.setViewsAndInitialize(tagViews);
     }
 
     private void setAllTags() {
